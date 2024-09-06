328665fbca46d11896f4ca309ecd1153478131dc
John Kim <johnjohn0927@gmail.com>
2024-09-06 13:14:55 -0700

    Midterm Milestone 1 Start commit

---
+++ Midterm Project/Midterm Project Milestone 1.py
@@ -0,0 +1,7 @@
+print("What is your Full Name?")
+fullName = input(" > ")
+print("What is your Year of Birth?")
+birthYear = int(input(" > "))
+currentYear = 2024
+age = currentYear - birthYear
+print("\nYour name is " + fullName + "and you are " + string(age) + " years old.")
